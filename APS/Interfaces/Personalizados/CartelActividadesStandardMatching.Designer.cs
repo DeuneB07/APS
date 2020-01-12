367d22fccc47cfd32bdb94afd328d644e0f3f601
jesuscumpli <jesuscumpli@uma.es>
2020-01-12 20:38:05 +0100

    Nuevo Interfaz Matching!

---
+++ APS/Interfaces/Personalizados/CartelActividadesStandardMatching.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace APS.Interfaces.Personalizados
+{
+    partial class CartelActividadesStandardMatching
+    {
+        /// <summary> 
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de componentes
+
+        /// <summary> 
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lShowResp = new System.Windows.Forms.Label();
+            this.lResp = new System.Windows.Forms.Label();
+            this.lShowOrg = new System.Windows.Forms.Label();
+            this.lShowIni = new System.Windows.Forms.Label();
+            this.lFecha = new System.Windows.Forms.Label();
+            this.lOrg = new System.Windows.Forms.Label();
+            this.lTipoActividad = new System.Windows.Forms.Label();
+            this.lTipoA = new System.Windows.Forms.Label();
+            this.bSolicitar = new System.Windows.Forms.Button();
+            this.lNumPlazas = new System.Windows.Forms.Label();
+            this.lPlazas = new System.Windows.Forms.Label();
+            this.labelName = new System.Windows.Forms.Label();
+            this.lDescripcion = new System.Windows.Forms.Label();
+            this.pict = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pict)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.lShowResp);
+            this.panel1.Controls.Add(this.lResp);
+            this.panel1.Controls.Add(this.lShowOrg);
+            this.panel1.Controls.Add(this.lShowIni);
+            this.panel1.Controls.Add(this.lFecha);
+            this.panel1.Controls.Add(this.lOrg);
+            this.panel1.Controls.Add(this.lTipoActividad);
+            this.panel1.Controls.Add(this.lTipoA);
+            this.panel1.Controls.Add(this.bSolicitar);
+            this.panel1.Controls.Add(this.lNumPlazas);
+            this.panel1.Controls.Add(this.lPlazas);
+            this.panel1.Controls.Add(this.labelName);
+            this.panel1.Controls.Add(this.lDescripcion);
+            this.panel1.Controls.Add(this.pict);
+            this.panel1.Location = new System.Drawing.Point(4, 4);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(831, 209);
+            this.panel1.TabIndex = 3;
+            // 
+            // lShowResp
+            // 
+            this.lShowResp.AutoSize = true;
+            this.lShowResp.Font = new System.Drawing.Font("Bahnschrift", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lShowResp.Location = new System.Drawing.Point(140, 71);
+            this.lShowResp.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lShowResp.Name = "lShowResp";
+            this.lShowResp.Size = new System.Drawing.Size(69, 17);
+            this.lShowResp.TabIndex = 19;
+            this.lShowResp.Text = "nounresp";
+            // 
+            // lResp
+            // 
+            this.lResp.AutoSize = true;
+            this.lResp.Font = new System.Drawing.Font("Bahnschrift", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lResp.Location = new System.Drawing.Point(20, 71);
+            this.lResp.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lResp.Name = "lResp";
+            this.lResp.Size = new System.Drawing.Size(93, 17);
+            this.lResp.TabIndex = 18;
+            this.lResp.Text = "Responsable:";
+            // 
+            // lShowOrg
+            // 
+            this.lShowOrg.AutoSize = true;
+            this.lShowOrg.Font = new System.Drawing.Font("Bahnschrift", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lShowOrg.Location = new System.Drawing.Point(111, 44);
+            this.lShowOrg.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lShowOrg.Name = "lShowOrg";
+            this.lShowOrg.Size = new System.Drawing.Size(62, 17);
+            this.lShowOrg.TabIndex = 17;
+            this.lShowOrg.Text = "nounorg";
+            // 
+            // lShowIni
+            // 
+            this.lShowIni.AutoSize = true;
+            this.lShowIni.Font = new System.Drawing.Font("Bahnschrift", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lShowIni.Location = new System.Drawing.Point(603, 102);
+            this.lShowIni.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lShowIni.Name = "lShowIni";
+            this.lShowIni.Size = new System.Drawing.Size(24, 17);
+            this.lShowIni.TabIndex = 16;
+            this.lShowIni.Text = "ini";
+            // 
+            // lFecha
+            // 
+            this.lFecha.AutoSize = true;
+            this.lFecha.Font = new System.Drawing.Font("Bahnschrift", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lFecha.Location = new System.Drawing.Point(544, 101);
+            this.lFecha.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lFecha.Name = "lFecha";
+            this.lFecha.Size = new System.Drawing.Size(49, 17);
+            this.lFecha.TabIndex = 14;
+            this.lFecha.Text = "Fecha:";
+            // 
+            // lOrg
+            // 
+            this.lOrg.AutoSize = true;
+            this.lOrg.Font = new System.Drawing.Font("Bahnschrift", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lOrg.Location = new System.Drawing.Point(20, 44);
+            this.lOrg.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lOrg.Name = "lOrg";
+            this.lOrg.Size = new System.Drawing.Size(67, 17);
+            this.lOrg.TabIndex = 13;
+            this.lOrg.Text = "Organiza:";
+            // 
+            // lTipoActividad
+            // 
+            this.lTipoActividad.AutoSize = true;
+            this.lTipoActividad.Font = new System.Drawing.Font("Bahnschrift", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lTipoActividad.Location = new System.Drawing.Point(676, 71);
+            this.lTipoActividad.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lTipoActividad.Name = "lTipoActividad";
+            this.lTipoActividad.Size = new System.Drawing.Size(52, 17);
+            this.lTipoActividad.TabIndex = 12;
+            this.lTipoActividad.Text = "tipoact";
+            // 
+            // lTipoA
+            // 
+            this.lTipoA.AutoSize = true;
+            this.lTipoA.Font = new System.Drawing.Font("Bahnschrift", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lTipoA.Location = new System.Drawing.Point(544, 71);
+            this.lTipoA.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lTipoA.Name = "lTipoA";
+            this.lTipoA.Size = new System.Drawing.Size(121, 17);
+            this.lTipoA.TabIndex = 11;
+            this.lTipoA.Text = "Tipo de Actividad:";
+            // 
+            // bSolicitar
+            // 
+            this.bSolicitar.BackColor = System.Drawing.Color.Transparent;
+            this.bSolicitar.Cursor = System.Windows.Forms.Cursors.Default;
+            this.bSolicitar.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.bSolicitar.Font = new System.Drawing.Font("Bahnschrift", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bSolicitar.ForeColor = System.Drawing.Color.Black;
+            this.bSolicitar.Location = new System.Drawing.Point(741, 166);
+            this.bSolicitar.Margin = new System.Windows.Forms.Padding(4);
+            this.bSolicitar.Name = "bSolicitar";
+            this.bSolicitar.Size = new System.Drawing.Size(85, 39);
+            this.bSolicitar.TabIndex = 10;
+            this.bSolicitar.Text = "Solicitar";
+            this.bSolicitar.UseVisualStyleBackColor = false;
+            // 
+            // lNumPlazas
+            // 
+            this.lNumPlazas.AutoSize = true;
+            this.lNumPlazas.BackColor = System.Drawing.Color.Transparent;
+            this.lNumPlazas.Font = new System.Drawing.Font("Bahnschrift", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lNumPlazas.Location = new System.Drawing.Point(681, 133);
+            this.lNumPlazas.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lNumPlazas.Name = "lNumPlazas";
+            this.lNumPlazas.Size = new System.Drawing.Size(36, 17);
+            this.lNumPlazas.TabIndex = 9;
+            this.lNumPlazas.Text = "num";
+            // 
+            // lPlazas
+            // 
+            this.lPlazas.AutoSize = true;
+            this.lPlazas.Font = new System.Drawing.Font("Bahnschrift", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lPlazas.Location = new System.Drawing.Point(544, 133);
+            this.lPlazas.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lPlazas.Name = "lPlazas";
+            this.lPlazas.Size = new System.Drawing.Size(135, 17);
+            this.lPlazas.TabIndex = 8;
+            this.lPlazas.Text = "Plazas Disponibles: ";
+            // 
+            // labelName
+            // 
+            this.labelName.Font = new System.Drawing.Font("Bahnschrift", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelName.Location = new System.Drawing.Point(4, 5);
+            this.labelName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(543, 28);
+            this.labelName.TabIndex = 4;
+            this.labelName.Text = "Nombre";
+            // 
+            // lDescripcion
+            // 
+            this.lDescripcion.Font = new System.Drawing.Font("Bahnschrift", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lDescripcion.Location = new System.Drawing.Point(188, 101);
+            this.lDescripcion.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lDescripcion.Name = "lDescripcion";
+            this.lDescripcion.Size = new System.Drawing.Size(355, 96);
+            this.lDescripcion.TabIndex = 5;
+            this.lDescripcion.Text = "Descripción";
+            // 
+            // pict
+            // 
+            this.pict.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pict.Image = global::APS.Properties.Resources.no_image;
+            this.pict.Location = new System.Drawing.Point(4, 101);
+            this.pict.Margin = new System.Windows.Forms.Padding(4);
+            this.pict.Name = "pict";
+            this.pict.Size = new System.Drawing.Size(176, 100);
+            this.pict.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pict.TabIndex = 6;
+            this.pict.TabStop = false;
+            // 
+            // CartelActividadesStandardMatching
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Teal;
+            this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "CartelActividadesStandardMatching";
+            this.Size = new System.Drawing.Size(839, 217);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pict)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.Label lDescripcion;
+        private System.Windows.Forms.PictureBox pict;
+        private System.Windows.Forms.Label lNumPlazas;
+        private System.Windows.Forms.Label lPlazas;
+        private System.Windows.Forms.Button bSolicitar;
+        private System.Windows.Forms.Label lTipoActividad;
+        private System.Windows.Forms.Label lTipoA;
+        private System.Windows.Forms.Label lShowOrg;
+        private System.Windows.Forms.Label lShowIni;
+        private System.Windows.Forms.Label lFecha;
+        private System.Windows.Forms.Label lOrg;
+        private System.Windows.Forms.Label lShowResp;
+        private System.Windows.Forms.Label lResp;
+    }
+}
---
+++ APS/Interfaces/Personalizados/CartelActividadesStandardMatching.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using APS.Mapeo;
+using APS.Interfaces.Gestión_Actividades;
+
+namespace APS.Interfaces.Personalizados
+{
+    public partial class CartelActividadesStandardMatching : UserControl
+    {
+
+        private Actividad a;
+        private Usuario user;
+
+        public CartelActividadesStandardMatching(Usuario user, Actividad a)
+        {
+            InitializeComponent();
+            this.a = a;
+            this.user = user;
+            this.labelName.Text = a.NombreAct;
+            this.lDescripcion.Text = a.DescAct;
+            this.lNumPlazas.Text = a.NumPlazas.ToString();
+            this.lTipoActividad.Text = a.TipoAct.ToString();
+            this.lShowIni.Text = a.FechaInicio.ToShortDateString() + " - " + a.FechaFin.ToShortDateString();
+            this.lShowOrg.Text = a.Organizador.Nombre;
+            if (a.Imagen != null) pict.Image = a.Imagen;
+
+            if (a.Responsable != null)
+            {
+                this.lShowResp.Text = a.Responsable.Nombre + " " + a.Responsable.Apellido1 + " " + a.Responsable.Apellido2;
+            } else
+            {
+                lResp.Visible = false;
+                lShowResp.Visible = false;
+            }
+        }
+
+        #region Properties
+        private string _noun;
+        private string _description;
+        private string _plazas;
+        private string _estado;
+        private string _tipoact;
+        private DateTime _fechaini;
+        private DateTime _fechafin;
+        private string _organizador;
+        private string _responsable;
+        private Image _icon;
+
+        [Category("Custom Props")]
+        public string Nombre
+        {
+            get { return _noun; }
+            set { _noun = value; labelName.Text = value; }
+        }
+
+        [Category("Custom Props")]
+        public string Description
+        {
+            get { return _description; }
+            set { _description = value; lDescripcion.Text = value; }
+        }
+
+        [Category("Custom Props")]
+        public string NumPlazas
+        {
+            get { return _plazas; }
+            set { _plazas = value; lNumPlazas.Text = value; }
+        }
+
+        [Category("Custom Props")]
+        public string TipoActividad
+        {
+            get { return _tipoact; }
+            set { _tipoact = value; lTipoActividad.Text = value; }
+        }
+
+        [Category("Custom Props")]
+        public DateTime FechaInicio
+        {
+            get { return _fechaini; }
+            set { _fechaini = value; lShowIni.Text = value.ToShortTimeString(); }
+        }
+
+        [Category("Custom Props")]
+        public string Organizador
+        {
+            get { return _organizador; }
+            set { _organizador = value; lShowOrg.Text = value; }
+        }
+
+        [Category("Custom Props")]
+        public string Responsable
+        {
+            get { return _responsable; }
+            set { _responsable = value; lShowResp.Text = value; }
+        }
+
+        [Category("Custom Props")]
+        public Image ImagenActividad
+        {
+            get { return _icon; }
+            set { _icon = value; pict.Image = value; }
+        }
+
+        #endregion
+
+        private void bVerMas_Click(object sender, EventArgs e)
+        {
+            VerActividadAbierta vAct = new VerActividadAbierta(this.user, this.a);
+            vAct.ShowDialog();
+        }
+    }
+}
