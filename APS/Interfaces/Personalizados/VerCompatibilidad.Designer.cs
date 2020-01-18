7a00e59a125787305ebf2c85f2c46e9444348df8
jesuscumpli <jesuscumpli@uma.es>
2020-01-18 18:14:50 +0100

    Last Changes!

---
+++ APS/Interfaces/Personalizados/VerCompatibilidad.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace APS.Interfaces.Personalizados
+{
+    partial class VerCompatibilidad
+    {
+        /// <summary> 
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de componentes
+
+        /// <summary> 
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(VerCompatibilidad));
+            this.checkBoxAdvTipoAct = new Syncfusion.Windows.Forms.Tools.CheckBoxAdv();
+            this.checkBoxAdvGrado = new Syncfusion.Windows.Forms.Tools.CheckBoxAdv();
+            this.checkBoxAdvAsig = new Syncfusion.Windows.Forms.Tools.CheckBoxAdv();
+            this.checkBoxAdvTurno = new Syncfusion.Windows.Forms.Tools.CheckBoxAdv();
+            this.checkBoxAdvHoras = new Syncfusion.Windows.Forms.Tools.CheckBoxAdv();
+            this.checkBoxAdvTipoTrabajo = new Syncfusion.Windows.Forms.Tools.CheckBoxAdv();
+            this.checkBoxAdvAmbitoTrabajo = new Syncfusion.Windows.Forms.Tools.CheckBoxAdv();
+            this.lblCompetencias = new System.Windows.Forms.Label();
+            this.lblShowCompetencias = new System.Windows.Forms.Label();
+            this.gradientPanel1 = new Syncfusion.Windows.Forms.Tools.GradientPanel();
+            ((System.ComponentModel.ISupportInitialize)(this.checkBoxAdvTipoAct)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.checkBoxAdvGrado)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.checkBoxAdvAsig)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.checkBoxAdvTurno)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.checkBoxAdvHoras)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.checkBoxAdvTipoTrabajo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.checkBoxAdvAmbitoTrabajo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gradientPanel1)).BeginInit();
+            this.gradientPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // checkBoxAdvTipoAct
+            // 
+            this.checkBoxAdvTipoAct.Border3DStyle = System.Windows.Forms.Border3DStyle.RaisedInner;
+            this.checkBoxAdvTipoAct.BorderColor = System.Drawing.SystemColors.HotTrack;
+            this.checkBoxAdvTipoAct.CheckedImage = ((System.Drawing.Image)(resources.GetObject("checkBoxAdvTipoAct.CheckedImage")));
+            this.checkBoxAdvTipoAct.Font = new System.Drawing.Font("Bahnschrift", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBoxAdvTipoAct.GradientEnd = System.Drawing.SystemColors.ButtonHighlight;
+            this.checkBoxAdvTipoAct.GradientStart = System.Drawing.SystemColors.ActiveCaptionText;
+            this.checkBoxAdvTipoAct.ImageCheckBox = true;
+            this.checkBoxAdvTipoAct.ImageCheckBoxSize = new System.Drawing.Size(16, 16);
+            this.checkBoxAdvTipoAct.Location = new System.Drawing.Point(16, 11);
+            this.checkBoxAdvTipoAct.Name = "checkBoxAdvTipoAct";
+            this.checkBoxAdvTipoAct.ShadowColor = System.Drawing.Color.Gray;
+            this.checkBoxAdvTipoAct.Size = new System.Drawing.Size(150, 21);
+            this.checkBoxAdvTipoAct.TabIndex = 1;
+            this.checkBoxAdvTipoAct.Text = "Tipo Actividad";
+            this.checkBoxAdvTipoAct.UncheckedImage = ((System.Drawing.Image)(resources.GetObject("checkBoxAdvTipoAct.UncheckedImage")));
+            // 
+            // checkBoxAdvGrado
+            // 
+            this.checkBoxAdvGrado.AutoCheck = false;
+            this.checkBoxAdvGrado.Border3DStyle = System.Windows.Forms.Border3DStyle.RaisedInner;
+            this.checkBoxAdvGrado.BorderColor = System.Drawing.SystemColors.HotTrack;
+            this.checkBoxAdvGrado.CheckedImage = ((System.Drawing.Image)(resources.GetObject("checkBoxAdvGrado.CheckedImage")));
+            this.checkBoxAdvGrado.Font = new System.Drawing.Font("Bahnschrift", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBoxAdvGrado.ImageCheckBox = true;
+            this.checkBoxAdvGrado.ImageCheckBoxSize = new System.Drawing.Size(16, 16);
+            this.checkBoxAdvGrado.Location = new System.Drawing.Point(16, 38);
+            this.checkBoxAdvGrado.Name = "checkBoxAdvGrado";
+            this.checkBoxAdvGrado.Size = new System.Drawing.Size(150, 21);
+            this.checkBoxAdvGrado.TabIndex = 2;
+            this.checkBoxAdvGrado.Text = "Grado";
+            this.checkBoxAdvGrado.UncheckedImage = ((System.Drawing.Image)(resources.GetObject("checkBoxAdvGrado.UncheckedImage")));
+            // 
+            // checkBoxAdvAsig
+            // 
+            this.checkBoxAdvAsig.AutoCheck = false;
+            this.checkBoxAdvAsig.Border3DStyle = System.Windows.Forms.Border3DStyle.RaisedInner;
+            this.checkBoxAdvAsig.BorderColor = System.Drawing.SystemColors.HotTrack;
+            this.checkBoxAdvAsig.CheckedImage = ((System.Drawing.Image)(resources.GetObject("checkBoxAdvAsig.CheckedImage")));
+            this.checkBoxAdvAsig.Font = new System.Drawing.Font("Bahnschrift", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBoxAdvAsig.ImageCheckBox = true;
+            this.checkBoxAdvAsig.ImageCheckBoxSize = new System.Drawing.Size(16, 16);
+            this.checkBoxAdvAsig.Location = new System.Drawing.Point(16, 85);
+            this.checkBoxAdvAsig.Name = "checkBoxAdvAsig";
+            this.checkBoxAdvAsig.Size = new System.Drawing.Size(150, 21);
+            this.checkBoxAdvAsig.TabIndex = 3;
+            this.checkBoxAdvAsig.Text = "Asignatura";
+            this.checkBoxAdvAsig.UncheckedImage = ((System.Drawing.Image)(resources.GetObject("checkBoxAdvAsig.UncheckedImage")));
+            // 
+            // checkBoxAdvTurno
+            // 
+            this.checkBoxAdvTurno.AutoCheck = false;
+            this.checkBoxAdvTurno.Border3DStyle = System.Windows.Forms.Border3DStyle.RaisedInner;
+            this.checkBoxAdvTurno.BorderColor = System.Drawing.SystemColors.HotTrack;
+            this.checkBoxAdvTurno.CheckedImage = ((System.Drawing.Image)(resources.GetObject("checkBoxAdvTurno.CheckedImage")));
+            this.checkBoxAdvTurno.Font = new System.Drawing.Font("Bahnschrift", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBoxAdvTurno.ImageCheckBox = true;
+            this.checkBoxAdvTurno.ImageCheckBoxSize = new System.Drawing.Size(16, 16);
+            this.checkBoxAdvTurno.Location = new System.Drawing.Point(16, 112);
+            this.checkBoxAdvTurno.Name = "checkBoxAdvTurno";
+            this.checkBoxAdvTurno.Size = new System.Drawing.Size(150, 21);
+            this.checkBoxAdvTurno.TabIndex = 4;
+            this.checkBoxAdvTurno.Text = "Turno";
+            this.checkBoxAdvTurno.UncheckedImage = ((System.Drawing.Image)(resources.GetObject("checkBoxAdvTurno.UncheckedImage")));
+            // 
+            // checkBoxAdvHoras
+            // 
+            this.checkBoxAdvHoras.AutoCheck = false;
+            this.checkBoxAdvHoras.Border3DStyle = System.Windows.Forms.Border3DStyle.RaisedInner;
+            this.checkBoxAdvHoras.BorderColor = System.Drawing.SystemColors.HotTrack;
+            this.checkBoxAdvHoras.CheckedImage = ((System.Drawing.Image)(resources.GetObject("checkBoxAdvHoras.CheckedImage")));
+            this.checkBoxAdvHoras.Font = new System.Drawing.Font("Bahnschrift", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBoxAdvHoras.ImageCheckBox = true;
+            this.checkBoxAdvHoras.ImageCheckBoxSize = new System.Drawing.Size(16, 16);
+            this.checkBoxAdvHoras.Location = new System.Drawing.Point(16, 139);
+            this.checkBoxAdvHoras.Name = "checkBoxAdvHoras";
+            this.checkBoxAdvHoras.Size = new System.Drawing.Size(150, 21);
+            this.checkBoxAdvHoras.TabIndex = 5;
+            this.checkBoxAdvHoras.Text = "Horas";
+            this.checkBoxAdvHoras.UncheckedImage = ((System.Drawing.Image)(resources.GetObject("checkBoxAdvHoras.UncheckedImage")));
+            // 
+            // checkBoxAdvTipoTrabajo
+            // 
+            this.checkBoxAdvTipoTrabajo.AutoCheck = false;
+            this.checkBoxAdvTipoTrabajo.Border3DStyle = System.Windows.Forms.Border3DStyle.RaisedInner;
+            this.checkBoxAdvTipoTrabajo.BorderColor = System.Drawing.SystemColors.HotTrack;
+            this.checkBoxAdvTipoTrabajo.CheckedImage = ((System.Drawing.Image)(resources.GetObject("checkBoxAdvTipoTrabajo.CheckedImage")));
+            this.checkBoxAdvTipoTrabajo.Font = new System.Drawing.Font("Bahnschrift", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBoxAdvTipoTrabajo.ImageCheckBox = true;
+            this.checkBoxAdvTipoTrabajo.ImageCheckBoxSize = new System.Drawing.Size(16, 16);
+            this.checkBoxAdvTipoTrabajo.Location = new System.Drawing.Point(16, 166);
+            this.checkBoxAdvTipoTrabajo.Name = "checkBoxAdvTipoTrabajo";
+            this.checkBoxAdvTipoTrabajo.Size = new System.Drawing.Size(150, 21);
+            this.checkBoxAdvTipoTrabajo.TabIndex = 6;
+            this.checkBoxAdvTipoTrabajo.Text = "Tipo Trabajo";
+            this.checkBoxAdvTipoTrabajo.UncheckedImage = ((System.Drawing.Image)(resources.GetObject("checkBoxAdvTipoTrabajo.UncheckedImage")));
+            // 
+            // checkBoxAdvAmbitoTrabajo
+            // 
+            this.checkBoxAdvAmbitoTrabajo.AutoCheck = false;
+            this.checkBoxAdvAmbitoTrabajo.Border3DStyle = System.Windows.Forms.Border3DStyle.RaisedInner;
+            this.checkBoxAdvAmbitoTrabajo.BorderColor = System.Drawing.SystemColors.HotTrack;
+            this.checkBoxAdvAmbitoTrabajo.CheckedImage = ((System.Drawing.Image)(resources.GetObject("checkBoxAdvAmbitoTrabajo.CheckedImage")));
+            this.checkBoxAdvAmbitoTrabajo.Font = new System.Drawing.Font("Bahnschrift", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBoxAdvAmbitoTrabajo.ImageCheckBox = true;
+            this.checkBoxAdvAmbitoTrabajo.ImageCheckBoxSize = new System.Drawing.Size(16, 16);
+            this.checkBoxAdvAmbitoTrabajo.Location = new System.Drawing.Point(16, 193);
+            this.checkBoxAdvAmbitoTrabajo.Name = "checkBoxAdvAmbitoTrabajo";
+            this.checkBoxAdvAmbitoTrabajo.Size = new System.Drawing.Size(150, 21);
+            this.checkBoxAdvAmbitoTrabajo.TabIndex = 7;
+            this.checkBoxAdvAmbitoTrabajo.Text = "Ambito Trabajo";
+            this.checkBoxAdvAmbitoTrabajo.UncheckedImage = ((System.Drawing.Image)(resources.GetObject("checkBoxAdvAmbitoTrabajo.UncheckedImage")));
+            // 
+            // lblCompetencias
+            // 
+            this.lblCompetencias.AutoSize = true;
+            this.lblCompetencias.BackColor = System.Drawing.Color.Transparent;
+            this.lblCompetencias.Font = new System.Drawing.Font("Bahnschrift", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCompetencias.Location = new System.Drawing.Point(59, 61);
+            this.lblCompetencias.Name = "lblCompetencias";
+            this.lblCompetencias.Size = new System.Drawing.Size(102, 18);
+            this.lblCompetencias.TabIndex = 8;
+            this.lblCompetencias.Text = "Competencias";
+            // 
+            // lblShowCompetencias
+            // 
+            this.lblShowCompetencias.AutoSize = true;
+            this.lblShowCompetencias.BackColor = System.Drawing.Color.Transparent;
+            this.lblShowCompetencias.Font = new System.Drawing.Font("Times New Roman", 10.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblShowCompetencias.Location = new System.Drawing.Point(12, 61);
+            this.lblShowCompetencias.Name = "lblShowCompetencias";
+            this.lblShowCompetencias.Size = new System.Drawing.Size(41, 20);
+            this.lblShowCompetencias.TabIndex = 9;
+            this.lblShowCompetencias.Text = "0/10";
+            // 
+            // gradientPanel1
+            // 
+            this.gradientPanel1.BackColor = System.Drawing.Color.White;
+            this.gradientPanel1.BackgroundColor = new Syncfusion.Drawing.BrushInfo(Syncfusion.Drawing.GradientStyle.PathEllipse, System.Drawing.Color.White, System.Drawing.Color.LightBlue);
+            this.gradientPanel1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.gradientPanel1.Controls.Add(this.lblShowCompetencias);
+            this.gradientPanel1.Controls.Add(this.checkBoxAdvTipoTrabajo);
+            this.gradientPanel1.Controls.Add(this.lblCompetencias);
+            this.gradientPanel1.Controls.Add(this.checkBoxAdvTipoAct);
+            this.gradientPanel1.Controls.Add(this.checkBoxAdvAmbitoTrabajo);
+            this.gradientPanel1.Controls.Add(this.checkBoxAdvGrado);
+            this.gradientPanel1.Controls.Add(this.checkBoxAdvAsig);
+            this.gradientPanel1.Controls.Add(this.checkBoxAdvHoras);
+            this.gradientPanel1.Controls.Add(this.checkBoxAdvTurno);
+            this.gradientPanel1.Location = new System.Drawing.Point(0, 0);
+            this.gradientPanel1.Name = "gradientPanel1";
+            this.gradientPanel1.Size = new System.Drawing.Size(178, 225);
+            this.gradientPanel1.TabIndex = 10;
+            // 
+            // Compatibilidad
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.gradientPanel1);
+            this.Name = "Compatibilidad";
+            this.Size = new System.Drawing.Size(178, 225);
+            ((System.ComponentModel.ISupportInitialize)(this.checkBoxAdvTipoAct)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.checkBoxAdvGrado)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.checkBoxAdvAsig)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.checkBoxAdvTurno)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.checkBoxAdvHoras)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.checkBoxAdvTipoTrabajo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.checkBoxAdvAmbitoTrabajo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gradientPanel1)).EndInit();
+            this.gradientPanel1.ResumeLayout(false);
+            this.gradientPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Syncfusion.Windows.Forms.Tools.CheckBoxAdv checkBoxAdvTipoAct;
+        private Syncfusion.Windows.Forms.Tools.CheckBoxAdv checkBoxAdvGrado;
+        private Syncfusion.Windows.Forms.Tools.CheckBoxAdv checkBoxAdvAsig;
+        private Syncfusion.Windows.Forms.Tools.CheckBoxAdv checkBoxAdvTurno;
+        private Syncfusion.Windows.Forms.Tools.CheckBoxAdv checkBoxAdvHoras;
+        private Syncfusion.Windows.Forms.Tools.CheckBoxAdv checkBoxAdvTipoTrabajo;
+        private Syncfusion.Windows.Forms.Tools.CheckBoxAdv checkBoxAdvAmbitoTrabajo;
+        private System.Windows.Forms.Label lblCompetencias;
+        private System.Windows.Forms.Label lblShowCompetencias;
+        private Syncfusion.Windows.Forms.Tools.GradientPanel gradientPanel1;
+    }
+}
